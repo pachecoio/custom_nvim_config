db49d467f8f28f010e1b142fdb9a659273c6b501
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2024-01-17 15:52:51 -0500

    Changed hop config to have more speed when using dvorak

--- lua/custom/plugins/hop.lua
+++ lua/custom/plugins/hop.lua
@@ -4,7 +4,7 @@ return {
     event = "BufRead",
     config = function()
       require("hop").setup()
-      vim.api.nvim_set_keymap("n", "<leader>h", ":HopWord<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "h", ":HopWord<cr>", { silent = true })
       -- vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
     end,
   },
