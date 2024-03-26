0df54b9a326333c87e619d1451690384ad631213
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2024-03-25 21:53:20 -0400

    Removed popup messages

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -3,12 +3,6 @@ return {
   event = "VeryLazy",
   opts = {
     -- add any options here
-    view = {
-      split = {
-        relative = "editor",
-        position = "bottom",
-      }
-    }
   },
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
@@ -18,27 +12,65 @@ return {
     --   If not available, we use `mini` as the fallback
     -- "rcarriga/nvim-notify",
   },
-  -- config = function()
-  --   require("noice").setup({
-  --     lsp = {
-  --       -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-  --       override = {
-  --         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-  --         ["vim.lsp.util.stylize_markdown"] = true,
-  --         ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-  --       },
-  --     },
-  --     -- you can enable a preset for easier configuration
-  --     -- presets = {
-  --     --   bottom_search = true,         -- use a classic bottom cmdline for search
-  --     --   command_palette = true,       -- position the cmdline and popupmenu together
-  --     --   long_message_to_split = true, -- long messages will be sent to a split
-  --     --   inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-  --     --   lsp_doc_border = false,       -- add a border to hover docs and signature help
-  --     -- },
-  --   })
-  --
-  --   -- dismiss with esc
-  --   vim.api.nvim_set_keymap("n", "<esc>", ":Noice dismiss<CR>", { noremap = true, silent = true })
-  -- end,
+  config = function()
+    require("noice").setup({
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+      -- you can enable a preset for easier configuration
+      -- presets = {
+      --   bottom_search = false,        -- use a classic bottom cmdline for search
+      --   command_palette = false,      -- position the cmdline and popupmenu together
+      --   long_message_to_split = true, -- long messages will be sent to a split
+      --   inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+      --   lsp_doc_border = false,       -- add a border to hover docs and signature help
+      -- },
+      views = {
+        cmdline_popup = {
+          relative = "editor",
+          position = {
+            row = 45,
+            col = "50%",
+          },
+          size = {
+            width = 60,
+            height = "auto",
+          },
+          border = {
+            style = "none",
+            padding = { 1, 2 },
+          },
+          win_options = {
+            winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+          },
+        },
+        popupmenu = {
+          relative = "editor",
+          position = {
+            row = 60,
+            col = "50%",
+          },
+          size = {
+            width = 60,
+            height = 10,
+          },
+          border = {
+            style = "none",
+            padding = { 1, 2 },
+          },
+          win_options = {
+            winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+          },
+        },
+      },
+    })
+
+    -- dismiss with esc
+    vim.api.nvim_set_keymap("n", "<esc>", ":Noice dismiss<CR>", { noremap = true, silent = true })
+  end,
 }
