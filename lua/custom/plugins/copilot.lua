49b6dfbd2df0a29713c28693c2c2d6cf6c409eb4
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2024-04-06 10:23:20 -0400

    Removed noice and changed copilot lib

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,36 +1,79 @@
 return {
+  -- {
+  --   "github/copilot.vim",
+  --   event = "VeryLazy",
+  --   config = function()
+  --     -- copilot assume mapped
+  --     vim.g.copilot_assume_mapped = true
+  --     vim.g.copilot_no_tab_map = true
+  --
+  --     -- Copilot config
+  --     local ok, copilot = pcall(require, "copilot")
+  --     if not ok then
+  --       return
+  --     end
+  --     copilot.setup {
+  --       suggestion = {
+  --         keymap = {
+  --           accept = "<c-l>",
+  --           next = "<c-j>",
+  --           prev = "<c-k>",
+  --           dismiss = "<c-h>",
+  --         },
+  --       },
+  --     }
+  --     local opts = { noremap = true, silent = true }
+  --     vim.api.nvim_set_keymap("n", "<c-s>", "<cmd>lua require('copilot.suggestion').toggle_auto_trigger()<CR>", opts)
+  --   end,
+  -- }
   {
-    "github/copilot.vim",
-    event = "VeryLazy",
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
     config = function()
-      -- copilot assume mapped
-      vim.g.copilot_assume_mapped = true
-      vim.g.copilot_no_tab_map = true
-
-      -- Copilot config
-      local ok, copilot = pcall(require, "copilot")
-      if not ok then
-        return
-      end
-      copilot.setup {
-        suggestion = {
+      require('copilot').setup({
+        panel = {
+          enabled = true,
+          auto_refresh = false,
           keymap = {
-            accept = "<c-l>",
-            next = "<c-j>",
-            prev = "<c-k>",
-            dismiss = "<c-h>",
+            jump_prev = "[[",
+            jump_next = "]]",
+            accept = "<TAB>",
+            refresh = "gr",
+            open = "<M-CR>"
+          },
+          layout = {
+            position = "bottom", -- | top | left | right
+            ratio = 0.4
           },
         },
-      }
-      local opts = { noremap = true, silent = true }
-      vim.api.nvim_set_keymap("n", "<c-s>", "<cmd>lua require('copilot.suggestion').toggle_auto_trigger()<CR>", opts)
-    end,
-  },
-  {
-    "hrsh7th/cmp-copilot",
-    config = function()
-      -- lvim.builtin.cmp.formatting.source_names["copilot"] = "( )"
-      -- table.insert(lvim.builtin.cmp.sources, 2, { name = "copilot" })
+        suggestion = {
+          enabled = true,
+          auto_trigger = false,
+          debounce = 75,
+          keymap = {
+            accept = "<TAB>",
+            accept_word = false,
+            accept_line = false,
+            next = "<c-j>",
+            prev = "<c-k>",
+            dismiss = "<ESC>",
+          },
+        },
+        filetypes = {
+          yaml = false,
+          markdown = false,
+          help = false,
+          gitcommit = false,
+          gitrebase = false,
+          hgcommit = false,
+          svn = false,
+          cvs = false,
+          ["."] = false,
+        },
+        copilot_node_command = 'node', -- Node.js version must be > 18.x
+        server_opts_overrides = {},
+      })
     end,
   }
 }
